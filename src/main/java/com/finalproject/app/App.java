3a6a8e010a4a15759168f31a2da008b2603200be
KidoAklilu <Kidoget321@gmail.com>
2025-04-10 15:22:17 -0400

    Fix welcome message typo

--- src/main/java/com/finalproject/app/App.java
+++ src/main/java/com/finalproject/app/App.java
@@ -2,7 +2,7 @@ package com.finalproject.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to Final Project!");
+        System.out.println("Welcome to Final Project! Updated bug 3");
         System.out.println("Hello endpoint test!");
         System.out.println("status endpoint test!");
     }
