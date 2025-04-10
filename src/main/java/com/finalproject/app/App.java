248f13636a8f58af1a40d0352228e19abad2b692
KidoAklilu <Kidoget321@gmail.com>
2025-04-10 14:54:49 -0400

    Add /hello endpoint

--- src/main/java/com/finalproject/app/App.java
+++ src/main/java/com/finalproject/app/App.java
@@ -3,5 +3,6 @@ package com.finalproject.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Final Project!");
+        System.out.println("Hello endpoint test!");
     }
 }
