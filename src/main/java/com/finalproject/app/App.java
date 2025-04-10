7d515010df6cb22701503500183cc82b637f80a9
KidoAklilu <Kidoget321@gmail.com>
2025-04-10 15:08:40 -0400

    Add /status endpoint to check app health

--- src/main/java/com/finalproject/app/App.java
+++ src/main/java/com/finalproject/app/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Final Project!");
         System.out.println("Hello endpoint test!");
+        System.out.println("status endpoint test!");
     }
 }
