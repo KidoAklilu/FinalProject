c2688d6019b1191250139f0fb5aa9beffffaeb0b
KidoAklilu <Kidoget321@gmail.com>
2025-04-11 21:42:25 -0400

    Fix Checkstyle issues

--- src/main/java/com/finalproject/app/App.java
+++ src/main/java/com/finalproject/app/App.java
@@ -1,6 +1,20 @@
 package com.finalproject.app;
 
+/**
+ * Main application class.
+ */
 public class App {
+
+    // Private constructor to prevent instantiation
+    private App() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    /**
+     * Entry point of the application.
+     *
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
         System.out.println("Welcome to Final Project! Updated bug 3");
         System.out.println("Hello endpoint test!");
